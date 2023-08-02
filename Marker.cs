33645c7ee705b4176b85f6679313b9090f887648
Ankit Kumar Jha <jhaankitkumar12@gmail.com>
2023-08-02 15:03:25 -0300

    Add files via upload

---
+++ Marker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Marker : MonoBehaviour
+{
+    public GameObject coffeeMug1;
+    public GameObject coffeeMug2;
+    public GameObject coffeeMug3;
+    public GameObject coffeeMug4;
+    public GameObject maze;
+    public GameObject redBear;
+    public GameObject clifford;
+    public GameObject chest;
+    public GameObject bubbleGum;
+    public GameObject crawley;
+    public GameObject bookshelf;
+    public GameObject curtains;
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
